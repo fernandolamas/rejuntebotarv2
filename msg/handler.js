9b48ea0ee781d53ea013506c1e52b9c4cc598432
Fernando <fernandolamasw@hotmail.com>
2021-05-24 17:46:08 -0300

    Added commands to server up and down in the bot

--- msg/handler.js
+++ msg/handler.js
@@ -1,6 +1,7 @@
-const {prefix} = require('../config/config.json');
+const { prefix } = require('../config/config.json');
 const aliases = require('../config/commands.json');
 const { addToQueue, leaveToQueue } = require('../functions/queue/queueFunctions');
+const { turnOnServer, turnOffServer } = require('../functions/server/serverFunctions')
 
 
 const handleMessage = (msg) => {
@@ -8,7 +9,7 @@ const handleMessage = (msg) => {
     var havePrefix = false;
 
     for (let index = 0; index < prefix.length; index++) {
-        if(msg.content.startsWith(prefix[index])) havePrefix=true;
+        if (msg.content.startsWith(prefix[index])) havePrefix = true;
     }
 
     if (!havePrefix || msg.author.bot || msg.channel.type === 'dm') return;
@@ -20,28 +21,64 @@ const handleMessage = (msg) => {
     // command = "maxsize"
     // args = ["2"]
 
-    if(aliases.addCommands.includes(command))
-    {
-        try{
+    if (aliases.addCommands.includes(command)) {
+        try {
             addToQueue(msg)
             return;
         }
-        catch(e){
+        catch (e) {
             console.log(e)
         }
         return;
     }
 
-    if(aliases.removeCommands.includes(command))
-    {
-        try{
+    if (aliases.removeCommands.includes(command)) {
+        try {
             leaveToQueue(msg)
             return;
         }
-        catch(e){
+        catch (e) {
             console.log(e)
         }
     }
+    if (aliases.serverUp.includes(command)) {
+        try {
+            const _servername = {
+                brasil,
+                uscentral,
+                useast                
+            }
+            
+            if(!args.includes(_servername)){
+                msg.channel.send("Server is not recognized by the bot");
+            }else{
+                turnOnServer();
+            }
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    if (aliases.serverDown.includes(command)) {
+        try {
+            const _servername = {
+                brasil,
+                uscentral,
+                useast                
+            }
+            
+            if(!args.includes(_servername)){
+                msg.channel.send("Server is not recognized by the bot");
+            }else{
+                turnOffServer();
+            }
+
+        } catch (e) {
+            console.log(e)
+        }
+        return;
+    }
 }
 
-module.exports = {handleMessage }
+module.exports = { handleMessage }
