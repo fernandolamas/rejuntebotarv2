27f6cd0f4f3311c891b6a5cc907b963bc41843c6
Wenceslao Ariel Citera <wencitera@gmail.com>
2021-05-24 11:43:14 -0300

    Funcionalidad de random para generar match

--- functions/generalFunctions.js
+++ functions/generalFunctions.js
@@ -3,7 +3,8 @@ function convertIDtoString(message,ids)
     var users = [];
     var client = message.client
     for (let index = 0; index < ids.length; index++) {
-        users.push(ids[index])
+        user = `<@!${ids[index]}>`
+        users.push(user)
     }
     return users;
 }
