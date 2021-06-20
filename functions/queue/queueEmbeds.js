3334acfcbf6b51af4f813f70ef950e739d9426af
Wenceslao Ariel Citera <wencitera@gmail.com>
2021-06-20 13:05:27 -0300

    Visible next match id in Queue

--- functions/queue/queueEmbeds.js
+++ functions/queue/queueEmbeds.js
@@ -1,15 +1,21 @@
 const config = require('../../config/config.json')
 const {convertIDtoString} = require('../generalFunctions')
 const Discord = require("discord.js");
+const path = './functions/match/matchTemplate.json';
+const fs = require('fs')
 
 function queueEmbed(message, queue)
 {
+    var file = fs.readFileSync(path);
+    let matchJson = JSON.parse(file);
+
     var players = convertIDtoString(message, queue);
 
     const queueEmbed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle(`Queue ${queue.length}/${config.matchsize}`)
-    .setDescription(players);
+    .setDescription(players)
+    .setFooter(`Next match: ${matchJson.id}`)
     message.channel.send(queueEmbed);
 }
 
