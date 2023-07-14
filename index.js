2f2f2c1a91883a362cc5d89285203207e7a350ff
Fernando <35930141+fernandolamas@users.noreply.github.com>
2023-07-14 12:15:01 -0300

    Log console to a file

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 let { init } = require('./discord/client.js');
 const express = require('express');
+const winston = require('winston');
 const app = express();
 const port = 3000;
-
-
-
+const fs = require('fs');
+const util = require('util');
+let d = new Date()
+let str = `${d.getDay()}-${d.getMonth()}-${d.getFullYear()}-${d.getHours()}-${d.getMinutes()}`;
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({ silent: process.env.NODE_ENV == "test" }),
+    new winston.transports.File({ filename: `logs/Full-${str}.log` }),
+  ],
+})
+const errorLogger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: `logs/Error-${str}.log` }),
+  ],
+})
+console.log = (...args) => {logger.log('info', ...args)}
+console.error = (...args) => {errorLogger.log('error', ...args)}
 const main = async () => {
   try {
     init(app, port);
